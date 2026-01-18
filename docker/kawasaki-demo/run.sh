0c98f24de8f95a011237494e876d50cb906b000b
Paul Draghicescu <pdragy@gmail.com>
2026-01-18 14:00:02 -0800

    update run deps for demos

--- docker/kawasaki-demo/run.sh
+++ docker/kawasaki-demo/run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-ROBOT=rs007l-b001
+# Usage: run.sh <robot_name>
+#
+# Start the kawasaki robot simulation and moveit.
+#
+# rs007l-b001,rs013n-a001,rs015x-a001,rs025n-a001,rs080n-a001,bx300l-b001,bxp135x-a001
+
+ROBOT=${1:-none}
 
 # Launch the controllers in the background
 nohup ros2 launch khi_hardware khi_bringup.launch.py robot:=$ROBOT simulation:=true &
